c00cd6724381b50545381b914aad1cdd67ae51da
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2023-09-10 00:07:47 +0530

    added potd

---
+++ september/SEP_10_insert_node_in_BST.cpp
@@ -0,0 +1,35 @@
+/*
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    Node *insert(Node *node, int data)
+    {
+
+        // Your code goes here
+        if (!node)
+        {
+            return new Node(data);
+        }
+        if (data < node->data)
+        {
+            node->left = insert(node->left, data);
+        }
+        else if (data > node->data)
+        {
+            node->right = insert(node->right, data);
+        }
+        return node;
+    }
+};
